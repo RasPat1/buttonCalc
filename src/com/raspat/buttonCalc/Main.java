1b0ad3503e8f23d7f7948b44720f088f6718b191
Rasesh Patel <RasPat1@gmail.com>
2012-09-10 04:37:10 -0400

    Rotation support, Calc Bug, TextView Init
    Added onInstanceSave and onRestoreInstance so that the values are
    protected during orientation changes and when leaving app onPause.
    Declared TextView as a class field then gave it an id after set content
    view in onCreate, do not have to findViewById multiple times, efficiency
    improvement.  Fixed minor calculation error that occured on
    initialization in which after selecting first operation first input was
    set to zero.

--- src/com/raspat/buttonCalc/Main.java
+++ src/com/raspat/buttonCalc/Main.java
@@ -3,6 +3,20 @@
  *Copyright 2012
  *
  *See ReadMe for notes
+ *
+ *validity checks are scattered all throughout the code... wtf. 
+ *
+ *BugLOG
+ ** Debug on actual device. 9-10-12 4:26AM
+ * 		
+ * 		thin white line appeared between title bar and notification bar 
+ * 		after switching form portrait to landscape and back.  Disappeared after swiping 
+ * 		down the notification shade or after about 5 seconds.
+ *
+ *		Toast comes in and out rather abruptly why?
+ *		
+ *		On turn the output value is not saved.  SHoudlnt' this happen automatically
+ *		since it happens automatically when we leave and return to the app?
  */
 package com.raspat.buttonCalc;
 
@@ -13,41 +27,54 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
-
+/**
+ 
+ * 
+ * 		
+ * @author RasPat
+ *
+ */
 public class Main extends Activity {
     //Every time a button is hit add that digit to the input once an operator is hit
     //move input to output and save the operator
     float output = 0;
     String input = "";
     char currentOperator = '0';				//Zero operator serves as start identifier
-    float memory1 = 0;
+    float memory1 = 0;						
+    TextView tv;
     
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);   
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
+    private final static String MEMORY_KEY = "mKey";
     
-    @Override
-    public void onPause() {
-    	debugEvent("onPause");
-//    	if (output)
-//    	memory1 = output;
-    	debugEvent("onPause", Float.toString(memory1));
-    	super.onPause();
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		tv = (TextView) findViewById(R.id.textView1);
+	}
 
-    public void onResume() {
-    	super.onResume();
-    	output = memory1;
-//    	display(output);
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+	
+//	@Override
+//	publice void onPause
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		outState.putCharSequence(MEMORY_KEY, tv.getText());
+		super.onSaveInstanceState(outState);
+	}
+	
+	@Override
+	protected void onRestoreInstanceState(Bundle savedInstanceState) {					//When we leave the app and come back it seems like 
+		super.onRestoreInstanceState(savedInstanceState);								//Most of the variables are stored and 
+		if (savedInstanceState != null
+				&& savedInstanceState.containsKey(MEMORY_KEY)) {
+			tv.setText(savedInstanceState.getCharSequence(MEMORY_KEY));
+		}
+	}
     public void calculate() {
     	if(!isValid(input)) {
 //    		showToast(getString(R.string.notanumber));
@@ -78,14 +105,16 @@ public class Main extends Activity {
 	 */
 	public void onClickOp(char op) {
 
-		if (input == "" && currentOperator == '=') {
+		if (currentOperator == '=' && input == "") {
 			output = memory1;
+		} else if (currentOperator == '0') {
+			output = isValid(input) ? Float.valueOf(input) : 0;
 		} else if (input != "") {
 			calculate();
 			display(output);
-			input = "";
 		}
 		currentOperator = op;
+		input = "";
 	}
     
 	public void onClickEquals(View v) {
@@ -110,8 +139,8 @@ public class Main extends Activity {
 		case R.id.button7: input +="7"; break;
 		case R.id.button8: input +="8"; break;
 		case R.id.button9: input +="9"; break;
-		case R.id.buttondecimal: input += !containsDecimal() ? "." :""; break; 
-		default: Log.d("debug", "The default onClick has been triggered"); break;
+		case R.id.buttondecimal: input += !containsDecimal() ? "." :""; break; 			//Should this be done here or elsewhere?
+		default: debugEvent("debug", "The default onClick has been triggered"); break;
 		}
 		display(input);
 	}
@@ -122,17 +151,15 @@ public class Main extends Activity {
 		case R.id.buttonsubtract: 	onClickOp('-'); break;
 		case R.id.buttonmultiply: 	onClickOp('*'); break;
 		case R.id.buttondivide: 	onClickOp('/'); break;
-		default: Log.d("debug", "The default onClickOp has been triggered"); break;
+		default: debugEvent("debug", "The default onClickOp has been triggered"); break;
 		}
 	}
 	
 	public void display(String s) {
-		TextView tv = (TextView) findViewById(R.id.textView1);
 		tv.setText(s);
 	}
 
 	public void display(float f) {
-		TextView tv = (TextView) findViewById(R.id.textView1);
 		tv.setText(String.valueOf(f)); 	// Doesn't need to be error checked
 										// right?
 	} 									// Maybe check for length or emptiness? Nah.
@@ -144,14 +171,14 @@ public class Main extends Activity {
 	 * This does not check for multiple decimals since that is checked when decimal button is pressed.
 	 */
 	public boolean isValid(String s) {
-		if (s == "" || s.length() == 0 || (s.length() == 1 && s.contains("."))
+		if (s == "" || s.length() == 0 || (s.length() == 1 && s.contains("."))		//isValid checks for emptiness should it do that?
 				|| (Float.valueOf(s)).isNaN()) {
 			return false;
 		}
 		return true;
 	}
 
-    public boolean containsDecimal() {
+    public boolean containsDecimal() {							//logic is wrong here
     	if(input.contains(".")) {
     		showToast(getString(R.string.duplicatedecimal));
     		return true;
@@ -160,7 +187,7 @@ public class Main extends Activity {
     }
     
 	public void showToast(String message) {
-		Toast.makeText(this, message ,Toast.LENGTH_LONG).show();
+		Toast.makeText(this, message ,Toast.LENGTH_SHORT).show();
 	}
     
     private void debugEvent(final String method) {
